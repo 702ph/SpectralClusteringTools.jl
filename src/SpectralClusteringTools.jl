048dc24bf3becdeef4897f520ef2d740047a9342
sh fu <shfu>
2024-11-28 22:50:35 +0100

    add sample docstring

--- src/SpectralClusteringTools.jl
+++ src/SpectralClusteringTools.jl
@@ -1,8 +1,12 @@
 module SpectralClusteringTools
 
-# Write your package code here.
+"""
+    timestwo(x)
+
+Multiply the input `x` by two.
+"""
 timestwo(x) = 2 * x
 
-export timestwo  
+export timestwo
 
 end
