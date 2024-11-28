5ca597d5bba4e2e53ffc0c401b05ca81196bb576
Documenter.jl <documenter@juliadocs.github.io>
2024-11-28 21:42:28 +0000

    build based on 8e0ba34

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpectralClusteringTools","category":"page"},{"location":"#SpectralClusteringTools","page":"Home","title":"SpectralClusteringTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectralClusteringTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpectralClusteringTools]","category":"page"}]
+}
