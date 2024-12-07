22f49c15dff46703fb5f056b3c95a3735caa5092
702ph <25045628+702ph@users.noreply.github.com>
2024-12-07 18:04:28 +0100

    Sh/feature sphere data without plot (#6)
    * add make_sphere
    
    
    ---------
    
    Co-authored-by: sh fu <shfu>

--- src/SpectralClusteringTools.jl
+++ src/SpectralClusteringTools.jl
@@ -5,16 +5,26 @@ using Statistics: mean, median
 using NearestNeighbors: KDTree, knn
 using Clustering: kmeans
 
+
 # import source files
+include("Utils/DataSets.jl")
 include("similarity_graphs_NJW.jl")
 include("affinity_matrix_NJW.jl")
 include("spectral_clustering_NJW.jl")
+
+
+"""
+    timestwo(x)
+
+Multiply the input `x` by two.
+"""
+timestwo(x) = 2 * x
 
 
 #export public functions
 export SpectralClusteringParams, construct_similarity_graph,
        spectral_clustering, epsilon_neighborhood_graph,
        knn_graph, mutual_knn_graph,
-       fully_connected_graph
+       fully_connected_graph, timestwo
 
 end
