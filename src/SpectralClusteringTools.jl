334674099ae3d666dfdfa715cbdc2a09e6017807
ran_zhang1128 <git@git.tu-berlin.de>
2025-01-19 16:33:26 +0100

    Implemented Self-Tuning Algorithm, currently works fine with concentric circles type data. Test scrpit and result in directory /src/Utils. For sphere type data, need further adaption.

--- src/SpectralClusteringTools.jl
+++ src/SpectralClusteringTools.jl
@@ -1,30 +1,52 @@
+"""
+SpectralClusteringTools.jl
+Main module file for spectral clustering implementations
+"""
 module SpectralClusteringTools
 
-using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen
-using Statistics: mean, median
+# 1. Import packages
+using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen, I, dot 
+using Statistics: mean, std, median
 using NearestNeighbors: KDTree, knn
 using Clustering: kmeans
+using ArnoldiMethod
+using SparseArrays
 
-
-# import source files
+# 2. Include implementations
+# Basic utilities
 include("Utils/DataSets.jl")
+
+# Self-tuning spectral clustering
+include("self_tuning.jl")
+
+# Traditional spectral clustering
 include("similarity_graphs_NJW.jl")
 include("affinity_matrix_NJW.jl")
 include("spectral_clustering_NJW.jl")
 
-
-"""
-    timestwo(x)
-
-Multiply the input `x` by two.
-"""
 timestwo(x) = 2 * x
 
+# 3. Exports
+export 
+    # Data generation
+    make_sphere,
+     
+    # Parameter types
+    SpectralClusteringParams,
+    SelfTuningParams,  
+    
+    # NJW spectral clustering
+    construct_similarity_graph,
+    spectral_clustering,
+    epsilon_neighborhood_graph,
+    knn_graph, 
+    mutual_knn_graph,
+    fully_connected_graph,
+    
+    # Self-tuning spectral clustering
+    construct_self_tuning_affinity,
+    self_tuning_spectral_clustering,
+    timestwo
+    
 
-#export public functions
-export SpectralClusteringParams, construct_similarity_graph,
-       spectral_clustering, epsilon_neighborhood_graph,
-       knn_graph, mutual_knn_graph,
-       fully_connected_graph, timestwo
-
-end
+end # module
