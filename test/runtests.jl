5b6ecd51d3f37fbb833f2079fc6dca3bed7258a7
Long Khanh <nguyenduong@campus.tu-berlin.de>
2025-01-20 09:40:41 +0100

    remove sample code remove sample code times_two()

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,4 @@ using SpectralClusteringTools
 using Test
 
 @testset "SpectralClusteringTools.jl" begin
-    @testset "timestwo" begin
-        @test timestwo(4.0) == 8.0
-        @test timestwo(2.0) != 8.0
-        # @test timestwo(3.0) == 8.0
-      end
 end
