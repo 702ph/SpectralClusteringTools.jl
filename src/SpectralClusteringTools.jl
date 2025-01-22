601c1b3d6f6baec609abf0beb1cab74413c81517
702ph <25045628+702ph@users.noreply.github.com>
2025-01-22 21:44:07 +0100

    resolve merge conflicts (into develop) (#46)
    * Sh/feature sphere data without plot (#3)
    
    * add make_sphere
    
    ---------
    
    Co-authored-by: sh fu <shfu>
    
    * Revert "Sh/feature sphere data without plot (#3)" (#5)
    
    This reverts commit d8d084f6ea73d70e6da55ef9228def6b2b98a803.
    
    * merge privious development to the main (#19)
    
    * Implement NJW Algorithm for Spectral Clustering (#4)
    
    * NJW Algorithm implemented
    
    ---------
    
    Co-authored-by: ran_zhang1128 <git@git.tu-berlin.de>
    
    * Sh/feature sphere data without plot (#6)
    
    * add make_sphere
    
    
    ---------
    
    Co-authored-by: sh fu <shfu>
    
    * Sh/feature documentation.jl (#7)
    
    * update top documentation page
    
    ---------
    
    Co-authored-by: sh fu <shfu>
    
    * Sh/feature readme (#18)
    
    * update readme
    
    * update index.md
    
    ---------
    
    Co-authored-by: sh fu <shfu>
    
    ---------
    
    Co-authored-by: LKKTWH <61924215+LKKTWH@users.noreply.github.com>
    Co-authored-by: ran_zhang1128 <git@git.tu-berlin.de>
    
    * fix link
    
    * fix missing part
    
    * fix dupilication
    
    ---------
    
    Co-authored-by: LKKTWH <61924215+LKKTWH@users.noreply.github.com>
    Co-authored-by: ran_zhang1128 <git@git.tu-berlin.de>
    Co-authored-by: sh fu <shfu>
    Co-authored-by: LongKhanhDuong <longkhanh_Duong@hotmail.de>
    Co-authored-by: shuya fuchigami <shfcgm702@gmail.com>

--- src/SpectralClusteringTools.jl
+++ src/SpectralClusteringTools.jl
@@ -1,5 +1,6 @@
 module SpectralClusteringTools
 
+
 using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen
 using Statistics: mean, median
 using NearestNeighbors: KDTree, knn
@@ -18,5 +19,5 @@ export SpectralClusteringParams, construct_similarity_graph,
        spectral_clustering, epsilon_neighborhood_graph,
        knn_graph, mutual_knn_graph,
        fully_connected_graph
-
+       
 end
