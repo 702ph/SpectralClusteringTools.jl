79e08e3a4c08574fa2509b2bcee0661d30f359b9
shuya fuchigami <shfcgm702@gmail.com>
2025-01-22 17:58:29 +0100

    fix dupilication

--- src/SpectralClusteringTools.jl
+++ src/SpectralClusteringTools.jl
@@ -1,17 +1,5 @@
 module SpectralClusteringTools
 
-using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen
-using Statistics: mean, median
-using NearestNeighbors: KDTree, knn
-using Clustering: kmeans
-
-
-# import source files
-include("Utils/DataSets.jl")
-include("similarity_graphs_NJW.jl")
-include("affinity_matrix_NJW.jl")
-include("spectral_clustering_NJW.jl")
-
 
 using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen
 using Statistics: mean, median
@@ -31,5 +19,5 @@ export SpectralClusteringParams, construct_similarity_graph,
        spectral_clustering, epsilon_neighborhood_graph,
        knn_graph, mutual_knn_graph,
        fully_connected_graph
-
+       
 end
