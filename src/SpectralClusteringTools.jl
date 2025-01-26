d37a4ab8745c35007a8258c78bf86e76edf95540
ran_zhang1128 <git@git.tu-berlin.de>
2025-01-26 21:45:54 +0100

    Fix lack of import, export, and using. Add interleaved moon-shaped data generation

--- src/SpectralClusteringTools.jl
+++ src/SpectralClusteringTools.jl
@@ -1,23 +1,55 @@
+"""
+SpectralClusteringTools.jl
+Main module file for spectral clustering implementations
+"""
 module SpectralClusteringTools
 
-
-using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen
-using Statistics: mean, median
+# 1. Import packages
+using LinearAlgebra: norm, eigvals, eigvecs, Diagonal, diag, Symmetric, eigen, I, dot 
+using Statistics: mean, std, median
 using NearestNeighbors: KDTree, knn
 using Clustering: kmeans
+using Plots
 
+# 2. Include implementations
 
-# import source files
-include("Utils/DataSets.jl")
+# Self-tuning spectral clustering
+include("self_tuning.jl")
+
+# Traditional spectral clustering
 include("similarity_graphs_NJW.jl")
 include("affinity_matrix_NJW.jl")
 include("spectral_clustering_NJW.jl")
 
+# Basic utilities
+include("Utils/DataSets.jl")
+include("Utils/2D_data_testing.jl")
 
-#export public functions
-export SpectralClusteringParams, construct_similarity_graph,
-       spectral_clustering, epsilon_neighborhood_graph,
-       knn_graph, mutual_knn_graph,
-       fully_connected_graph
-       
-end
+timestwo(x) = 2 * x
+
+# 3. Exports
+export 
+    # Data generation
+    make_sphere,
+    generate_mixed_concentric_data,
+    run_mixed_circle_test,
+     
+    # Parameter types
+    SpectralClusteringParams,
+    SelfTuningParams,  
+    
+    # NJW spectral clustering
+    construct_similarity_graph,
+    spectral_clustering,
+    epsilon_neighborhood_graph,
+    knn_graph, 
+    mutual_knn_graph,
+    fully_connected_graph,
+    
+    # Self-tuning spectral clustering
+    construct_self_tuning_affinity,
+    self_tuning_spectral_clustering,
+    timestwo
+    
+
+end # module
